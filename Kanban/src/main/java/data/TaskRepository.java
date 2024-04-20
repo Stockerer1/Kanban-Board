9b48a5d5da45cab5aa2b82051c1e0b8c154d3af6
Stockerer1 <151563816+Stockerer1@users.noreply.github.com>
2024-04-20 13:49:17 +0200

    added

--- Kanban/src/main/java/data/TaskRepository.java
+++ Kanban/src/main/java/data/TaskRepository.java
@@ -1,20 +1,35 @@
 package data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskRepository {
 
     private List<Task> tasks;
 
-    public TaskRepository(List<Task> tasks) {
-        this.tasks = tasks;
+    public TaskRepository() {
+        this.tasks = new ArrayList<>();
     }
 
-    public List<Task> getTasks() {
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
+
+    public void updateTask(Task taskToUpdate) {
+        for (int i = 0; i < tasks.size(); i++) {
+            Task task = tasks.get(i);
+            if (task.getTaskId() == taskToUpdate.getTaskId()) {
+                tasks.set(i, taskToUpdate);
+                break;
+            }
+        }
+    }
+
+    public void deleteTask(int taskId) {
+        tasks.removeIf(task -> task.getTaskId() == taskId);
+    }
+
+    public List<Task> getAllTasks() {
         return tasks;
-    }
-
-    public void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
     }
 }
