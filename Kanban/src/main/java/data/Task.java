5fd8eeb633529ff4eaf6ca3ee382e7aa1ee9b54a
Stockerer1 <151563816+Stockerer1@users.noreply.github.com>
2024-04-20 11:35:06 +0200

    add java project

---
+++ Kanban/src/main/java/data/Task.java
@@ -0,0 +1,58 @@
+package data;
+
+public class Task {
+
+    String title;
+    String description;
+    String taskID;
+    String labels;
+    String status;
+
+    public Task(String title, String description, String taskID, String labels, String status) {
+        this.title = title;
+        this.description = description;
+        this.taskID = taskID;
+        this.labels = labels;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTaskID() {
+        return taskID;
+    }
+
+    public void setTaskID(String taskID) {
+        this.taskID = taskID;
+    }
+
+    public String getLabels() {
+        return labels;
+    }
+
+    public void setLabels(String labels) {
+        this.labels = labels;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ Kanban/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        System.out.println("You added the number: "+number);
+
+}
